8772db2a896c112a430563d605c4790096032518
Zachary Miller <Zachary.Miller2@marist.edu>
2014-11-14 10:38:38 -0500

    update

--- game.js
+++ game.js
@@ -1,1 +1,45 @@
-var test = 0;
+var player = {
+  //name: John Jay,
+  /*
+  function pickup(item){
+    this.items.push (item);
+  }
+  
+  function drop (item){
+    //this.items
+  }
+  */
+};
+
+function interpret (input){
+  console.log("interpret");
+}
+
+function execute (object){
+  console.log("execute");
+}
+
+function report () {
+  console.log("report");
+}
+
+function gameStep (inputString){
+  console.log("gameStep");
+  var command = interpret(input);
+  execute(command);
+  report();
+  
+}
+
+function gameStart(){
+  console.log("The Game has started");
+  var inputBox = document.querySelector("input");
+  inputBox.addEventListener ("keyup",function (event){
+    if (event.keycode === 13){
+      var inputText = this.value;
+      gameStep(inputText);
+    }
+  });
+}
+
+window.onload = gameStart;
